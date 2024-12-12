7dd3bfe015d5ae489b7fe8f9a905a74f264b41c6
Hasan-Vural <108330009+Hasan-Vural@users.noreply.github.com>
2024-12-12 17:18:34 +0300

    Created Movie Add, and some changes

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from login import Login
 from register import Register
 from homePage import HomePage
 from tkinter import *
+from userProfile import UserProfile
 
 def main():
     root=Tk()
     root.title("Pyflix")
     root.geometry("1920x1080")
 
-    HomePage(root)
+    UserProfile(root)
 
     root.mainloop()
 
