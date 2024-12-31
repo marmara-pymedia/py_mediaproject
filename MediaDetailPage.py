022b2f6c9e099566b872b7dcf17053cbd5682b64
syeliz <yelizkurtt4@gmail.com>
2024-12-31 20:44:18 +0300

    Deleted unnecessary Jsons from mediDetail

--- MediaDetailPage.py
+++ MediaDetailPage.py
@@ -314,31 +314,6 @@ class mediaDetail:
     def delete_media(self):
         print("Delete Media")
     
-    # will add somehtin 
-    
-    def get_user_choice(self):
-        with open('user.json', 'r') as user_file:
-            user_data = json.load(user_file)
-            return user_data['selected_media_id']
-
-    def get_media_image(self, media_id):
-        with open('media.json', 'r') as media_file:
-            media_data = json.load(media_file)
-            for media in media_data['medias']:
-                if media['id'] == media_id:
-                    return media['image_path']
-        return None
-
-    def update_media_label(self):
-        media_id = self.get_user_choice()
-        image_path = self.get_media_image(media_id)
-        if image_path:
-            new_image = PhotoImage(file=image_path)
-            self.mediaLabel = Label(self.bgFrame, image=new_image)
-            self.mediaLabel.img_reference = new_image
-            self.mediaLabel.pack(fill=BOTH, pady=(95))
-
-
 
 root=Tk()
 root.title("Media Detail Page")
